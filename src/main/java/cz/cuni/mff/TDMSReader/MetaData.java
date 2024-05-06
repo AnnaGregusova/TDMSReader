d875e6c9e77910762646fff92e37df024f98d7b1
Anna Gregušová <annagregusova@Anna-MacBook.local>
2024-05-06 07:59:21 +0200

    Add tdmsFIleInfo

--- src/main/java/cz/cuni/mff/TDMSReader/MetaData.java
+++ src/main/java/cz/cuni/mff/TDMSReader/MetaData.java
@@ -4,14 +4,17 @@ import java.util.List;
 public class MetaData{
     private int numberOfObjects;
     private ArrayList<TDMSGroup> groups;
+    private ArrayList<Property> tdmsFileInfo;
    // private List<TDMSChannel> channels;
 
     public int getNumberOfObjects() { return numberOfObjects; }
+
+    public ArrayList<Property> getTdmsFileInfo() {return tdmsFileInfo; }
     public ArrayList<TDMSGroup> getGroups() {return groups;}
     //public List<TDMSChannel> getChannels(){return channels;}
-    public MetaData(int numberOfObjects, ArrayList<TDMSGroup> groups){ //, List<TDMSGroup> groups, List<TDMSChannel> channels){
-        this.numberOfObjects = numberOfObjects;
+    public MetaData(ArrayList<TDMSGroup> groups, ArrayList<Property> tdmsFileInfo){ //, List<TDMSGroup> groups, List<TDMSChannel> channels){
         this.groups = groups;
+        this.tdmsFileInfo = tdmsFileInfo;
         //this.channels = channels;
     }
 
@@ -32,6 +35,13 @@ class Property{
                 '}';
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public Object getPropertyValue() {
+        return propertyValue;
+    }
 }
 
 
