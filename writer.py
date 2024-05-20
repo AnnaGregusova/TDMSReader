e6e6c583d638227269ee015d954e8ad3314ab983
Anna Gregušová <annagregusova@Anna-MacBook.local>
2024-05-20 11:27:43 +0200

    Remove unnecessarz files

--- writer.py
+++
@@ -1,8 +0,0 @@
-rom nptdms import TdmsWriter, ChannelObject
-import numpy
-
-with TdmsWriter("example.tdms") as tdms_writer:
-    data_array = numpy.linspace(0, 1, 10)
-    print(data_array)
-    channel = ChannelObject('group name', 'channel name', data_array)
-    tdms_writer.write_segment([channel])
