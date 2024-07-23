cac56e53dbdc043e4884a16cdc7458b2bbc707bd
Anna Gregušová <annagregusova@anna-macbook.ph.emea.bosch.com>
2024-07-23 11:21:16 +0200

    Add methods for reading added tdms files

--- src/main/java/cz/cuni/mff/TDMSReader/Main.java
+++ src/main/java/cz/cuni/mff/TDMSReader/Main.java
@@ -52,7 +52,19 @@ public class Main {
                     case "path8":
                         readPath8();
                         break;
-                    default:
+		   case "path9":
+			readPath9();
+			break;
+		   case "path10":
+			readPath10();
+			break;
+		   case "path11":
+                        readPath11();
+                        break;
+		   case "path12":
+			readPath12();
+			break;
+                   default:
                         System.out.println("Invalid path. Please choose one of the listed paths.");
                         break;
                 }
@@ -297,4 +309,109 @@ public class Main {
         TDMSFile tdmsFile = TDMSFile.read(path);
 
     }
+     /**
+     * Reads and processes the TDMS file specified in the path9.
+     * It displays the file, its properties, groups, and channel data.
+     *
+     * @throws IOException If there is an issue reading the TDMS file.
+     */
+    private static void readPath9() throws IOException{
+
+        String path = "tdms_files/H2_test_20240229_113102_0001 (1).tdms";
+
+        TDMSFile tdmsFile = TDMSFile.read(path);
+        System.out.println("TDMs file leadindata: ");
+        tdmsFile.printLeadInData();
+        System.out.println("TDMs file metadata: ");
+        tdmsFile.printMetaData();
+
+
+        System.out.println("TDMs File: " + tdmsFile);
+
+        System.out.println("TDMs file properties: " + tdmsFile.getProperties());
+
+        ArrayList<TDMSGroup> groups = tdmsFile.getGroups();
+        System.out.println("File groups: " + groups);
+      
+        System.out.println("Choose which path do you want to read: " +
+                "path1, path2, path3, path4, path5, path6. Type 'exit' to quit.");
+
+    }
+    
+    private static void readPath10() throws IOException{ //pada na Java heap space nejspis proto ze je tam hodne channelu
+
+        String path = "tdms_files/E2300114-09_ED2303836_2_2024-03-12_115045.tdms";
+
+        TDMSFile tdmsFile = TDMSFile.read(path);
+        System.out.println("TDMs file leadindata: ");
+        tdmsFile.printLeadInData();
+        System.out.println("TDMs file metadata: ");
+        tdmsFile.printMetaData();
+
+        System.out.println("TDMs File: " + tdmsFile);
+
+        System.out.println("TDMs file properties: " + tdmsFile.getProperties());
+
+        ArrayList<TDMSGroup> groups = tdmsFile.getGroups();
+        System.out.println("File groups: " + groups);
+      
+        System.out.println("Choose which path do you want to read: " +
+                "path1, path2, path3, path4, path5, path6. Type 'exit' to quit.");
+
+    }
+    private static void readPath11() throws IOException{ //totez jak u 10
+        String path = "tdms_files/E2300282-07_ED2404273_0_2024-03-27_093509.tdms";
+
+        TDMSFile tdmsFile = TDMSFile.read(path);
+        System.out.println("TDMs file leadindata: ");
+        tdmsFile.printLeadInData();
+        System.out.println("TDMs file metadata: ");
+        tdmsFile.printMetaData();
+
+
+        System.out.println("TDMs File: " + tdmsFile);
+
+        System.out.println("TDMs file properties: " + tdmsFile.getProperties());
+
+        ArrayList<TDMSGroup> groups = tdmsFile.getGroups();
+        System.out.println("File groups: " + groups);
+      
+        System.out.println("Choose which path do you want to read: " +
+                "path1, path2, path3, path4, path5, path6. Type 'exit' to quit.");
+
+    }
+    private static void readPath12() throws IOException{ //tady je nejak mo cdivne segmentu
+
+        String path = "tdms_files/test_TDMS_20240404_103108_0009.tdms";
+
+        TDMSFile tdmsFile = TDMSFile.read(path);
+        System.out.println("TDMs file leadindata: ");
+        ///tdmsFile.printLeadInData();
+        System.out.println("TDMs file metadata: ");
+        //tdmsFile.printMetaData();
+
+
+        System.out.println("TDMs File: " + tdmsFile);
+
+        System.out.println("TDMs file properties: " + tdmsFile.getProperties());
+
+        ArrayList<TDMSGroup> groups = tdmsFile.getGroups();
+        System.out.println("File groups: " + groups);
+        TDMSGroup tdmsGroup = tdmsFile.getGroup("/'AI channels'");
+
+        System.out.println("Group properties: ");
+        ArrayList<TDMSProperty> groupProperties = tdmsGroup.getProperties();
+        System.out.println(groupProperties);
+        ArrayList<TDMSChannel> channels = tdmsGroup.getChannels();
+        System.out.println(channels);
+        TDMSChannel tdmsChannel = tdmsGroup.getChannel("/'AI channels'/'-AI-Presure_Inlet_02'");
+        System.out.println("Raw data of a channel" + tdmsChannel.getName());
+        ArrayList<Object> rawData = tdmsChannel.getRawData();
+        System.out.println(rawData);
+      
+        System.out.println("Choose which path do you want to read: " +
+                "path1, path2, path3, path4, path5, path6. Type 'exit' to quit.");
+
+    }
+
 }
